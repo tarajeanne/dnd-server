b28418b1f8083851415c1f6aa2ec2819f2cf83cf
tarajeanne <t.jeanne.patel@gmail.com>
2019-11-18 09:33:16 -0600

    refactored gnome to proper format

--- src/stores/raceStore.js
+++ src/stores/raceStore.js
@@ -53,7 +53,7 @@ const raceStore = [
         name: '',
         variable: true,
         depends_on: 'race',
-        options: ['smith\'s tools', 'brewer\'s supplies', 'mason\'s tools']
+        options: ["smith's tools", "brewer's supplies", "mason's tools"]
       },
       {
         name: 'Stonecunning',
@@ -133,7 +133,7 @@ const raceStore = [
       {
         name: 'Fey Ancestry',
         desc:
-          'You have advantage on saving throws against being charmed, and magic can\'t put you to sleep.',
+          "You have advantage on saving throws against being charmed, and magic can't put you to sleep.",
         depends_on: 'race',
         variable: false
       }
@@ -180,12 +180,14 @@ const raceStore = [
     name: 'Halfling',
     desc:
       ' Halfling TraitsYour halfling character has a number of traits in common with all other halflings.',
-    asi: [{
-      name: 'Dexterity',
-      variable: false,
-      depends_on: 'race',
-      magnitude: 2
-    }],
+    asi: [
+      {
+        name: 'Dexterity',
+        variable: false,
+        depends_on: 'race',
+        magnitude: 2
+      }
+    ],
     weap_prof: [],
     age: 'Adults at 20, they live until about 150.',
     alignment:
@@ -193,8 +195,7 @@ const raceStore = [
     size: 'Small',
     size_desc: 'Average about 3 feet tall and weigh 40 lbs.',
     speed: 25,
-    languages:
-    [
+    languages: [
       {
         name: 'Halfling',
         variable: false,
@@ -214,13 +215,15 @@ const raceStore = [
         name: 'Lucky',
         variable: false,
         depends_on: 'race',
-        desc: 'When you roll a 1 on the d20 for an attack roll, ability check, or saving throw, you can reroll the die and must use the new roll'
+        desc:
+          'When you roll a 1 on the d20 for an attack roll, ability check, or saving throw, you can reroll the die and must use the new roll'
       },
       {
         name: 'Halfling Nimbleness',
         depends_on: 'race',
         variable: false,
-        desc: 'You can move through the space of any creature that is of a size larger than yours.'
+        desc:
+          'You can move through the space of any creature that is of a size larger than yours.'
       }
     ],
     subraces: [
@@ -241,7 +244,7 @@ const raceStore = [
     id: 4,
     name: 'Human',
     desc:
-      'The youngest and most prolific race of them all, humans don\'t have many general traits.',
+      "The youngest and most prolific race of them all, humans don't have many general traits.",
     asi: [
       {
         name: 'Strength',
@@ -316,7 +319,7 @@ const raceStore = [
     weap_prof: [],
     age: 'Adults by 15, they live to be about 80.',
     alignment:
-      'Tending toward extreme, they\'re generally very good--but when they\'re evil, they\'re evil.',
+      "Tending toward extreme, they're generally very good--but when they're evil, they're evil.",
     size: 'Medium',
     size_desc: 'Well over 6 feet tall and averaging 250 lbs.',
     speed: 30,
@@ -342,12 +345,14 @@ const raceStore = [
     name: 'Gnome',
     desc:
       ' Gnome TraitsYour gnome character has certain characteristics in common with all other gnomes.',
-    asi: [{
-      name: 'Intelligence',
-      variable: false,
-      depends_on: 'race',
-      magnitude: 2
-    }],
+    asi: [
+      {
+        name: 'Intelligence',
+        variable: false,
+        depends_on: 'race',
+        magnitude: 2
+      }
+    ],
     weap_prof: [],
     age: 'Settle as adults by 40m and live between 350 and 500 years.',
     alignment:
@@ -355,19 +360,30 @@ const raceStore = [
     size: 'Small',
     size_desc: 'Between 3 and 4 feet tall, averaging 40 lbs.',
     speed: 25,
-    languages:  [
+    languages: [
       {
         name: 'Gnomish',
         variable: false,
         depends_on: 'race'
       }
     ],
-    traits: {
-      Darkvision:
-        'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.',
-      'Gnome Cunning':
-        'You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic.'
-    },
+    other_prof: [
+      {
+        name: 'Gnome Cunning',
+        depends_on: 'race',
+        variable: false,
+        desc: 'You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic.'
+      }
+    ],
+    skills_and_features: [
+      {
+        name: 'Darkvision',
+        variable: false,
+        depends_on: 'race',
+        desc: 'You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light.'
+      }
+    
+    ],
     subraces: [
       {
         name: 'Rock Gnome',
@@ -391,26 +407,28 @@ const raceStore = [
     name: 'Half-Elf',
     desc:
       ' Half-Elf TraitsYour half-elf character has some qualities in common with elves and some that are unique to half-elves.',
-    asi: [{
-      name: 'Charisma',
-      variable: false,
-      depends_on: 'race',
-      magnitude: 2
-    },
-    {
-      name: '',
-      variable: true,
-      depends_on: 'race',
-      magnitude: 1,
-      norepeat: true
-    },
-    {
-      name: '',
-      variable: true,
-      depends_on: 'race',
-      magnitude: 1,
-      norepeat: true
-    }],
+    asi: [
+      {
+        name: 'Charisma',
+        variable: false,
+        depends_on: 'race',
+        magnitude: 2
+      },
+      {
+        name: '',
+        variable: true,
+        depends_on: 'race',
+        magnitude: 1,
+        norepeat: true
+      },
+      {
+        name: '',
+        variable: true,
+        depends_on: 'race',
+        magnitude: 1,
+        norepeat: true
+      }
+    ],
     weap_prof: [],
     age: 'Adults in their early 20s, they can live 180 years.',
     alignment:
@@ -418,7 +436,7 @@ const raceStore = [
     size: 'Medium',
     size_desc: 'Range from 5 to 6 feet tall.',
     speed: 30,
-    languages:  [
+    languages: [
       {
         name: 'Elvish',
         variable: false,
@@ -452,12 +470,13 @@ const raceStore = [
     },
     subraces: []
   },
-    {
-      id: 8,
-      name: 'Half-Orc',
-      desc:
-        ' Half-Orc TraitsYour half-orc character has certain traits deriving from your orc ancestry.',
-      asi: [{
+  {
+    id: 8,
+    name: 'Half-Orc',
+    desc:
+      ' Half-Orc TraitsYour half-orc character has certain traits deriving from your orc ancestry.',
+    asi: [
+      {
         name: 'Strength',
         magnitude: 2,
         variable: false,
@@ -468,41 +487,47 @@ const raceStore = [
         magnitude: 1,
         variable: false,
         depends_on: 'race'
-      }],
-      weap_prof: [],
-      age: 'Adults at 14, rarely living longer than 75 years.',
-      alignment:
-        'Orcs have a tendency toward chaos and are not particularly inclined toward good.',
-      size: 'Medium',
-      size_desc:
-        'Larger and bulkier than humans, ranging from 5 to well over 6 feet tall.',
-      speed: 30,
-      languages: [{
+      }
+    ],
+    weap_prof: [],
+    age: 'Adults at 14, rarely living longer than 75 years.',
+    alignment:
+      'Orcs have a tendency toward chaos and are not particularly inclined toward good.',
+    size: 'Medium',
+    size_desc:
+      'Larger and bulkier than humans, ranging from 5 to well over 6 feet tall.',
+    speed: 30,
+    languages: [
+      {
         name: 'Orc',
         variable: false,
         depends_on: 'race'
-      }],
-      check_prof: [{
+      }
+    ],
+    check_prof: [
+      {
         name: 'Intimidation',
         variable: false,
-        depends_on: 'race',
-      }],
-      traits: {
-        'Relentless Endurance':
-          'When you are reduced to 0 hit points but not killed outright, you can drop to 1 hit point instead. You can’t use this feature again until you finish a long rest.',
-        'Savage Attacks':
-          'When you score a critical hit with a melee weapon attack, you can roll one of the weapon’s damage dice one additional time and add it to the extra damage of the critical hit.',
-        Darkvision:
-          'Darkvision. Thanks to your orc blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.'
-      },
-      subraces: []
+        depends_on: 'race'
+      }
+    ],
+    traits: {
+      'Relentless Endurance':
+        'When you are reduced to 0 hit points but not killed outright, you can drop to 1 hit point instead. You can’t use this feature again until you finish a long rest.',
+      'Savage Attacks':
+        'When you score a critical hit with a melee weapon attack, you can roll one of the weapon’s damage dice one additional time and add it to the extra damage of the critical hit.',
+      Darkvision:
+        'Darkvision. Thanks to your orc blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.'
     },
-    {
-      id: 9,
-      name: 'Tiefling',
-      desc:
-        ' Tiefling TraitsTieflings share certain racial traits as a result of their infernal descent.',
-      asi: [{
+    subraces: []
+  },
+  {
+    id: 9,
+    name: 'Tiefling',
+    desc:
+      ' Tiefling TraitsTieflings share certain racial traits as a result of their infernal descent.',
+    asi: [
+      {
         name: 'Intelligence',
         magnitude: 1,
         variable: false,
@@ -513,27 +538,30 @@ const raceStore = [
         magnitude: 2,
         variable: false,
         depends_on: 'race'
-      }],
-      weap_prof: [],
-      age: 'Tieflings mature around 20 and live around 100 years.',
-      alignment: 'Most tieflings tend toward chaotic evil.',
-      size: 'Medium',
-      size_desc: 'Same size and build as humans.',
-      speed: 30,
-      languages: [{
+      }
+    ],
+    weap_prof: [],
+    age: 'Tieflings mature around 20 and live around 100 years.',
+    alignment: 'Most tieflings tend toward chaotic evil.',
+    size: 'Medium',
+    size_desc: 'Same size and build as humans.',
+    speed: 30,
+    languages: [
+      {
         name: 'Infernal',
         variable: false,
         depends_on: 'race'
-      }],
-      traits: {
-        'Hellish Resistance': 'You have resistance to fire damage.',
-        'Infernal Legacy':
-          'You know the *thaumaturgy* cantrip. When you reach 3rd level, you can cast the *hellish rebuke* spell as a 2nd-level spell once with this trait and regain the ability to do so when you finish a long rest. When you reach 5th level, you can cast the *darkness* spell once with this trait and regain the ability to do so when you finish a long rest. Charisma is your spellcasting ability for these spells.',
-        Darkvision:
-          'Thanks to your infernal heritage, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.'
-      },
-      subraces: []
-    }
+      }
+    ],
+    traits: {
+      'Hellish Resistance': 'You have resistance to fire damage.',
+      'Infernal Legacy':
+        'You know the *thaumaturgy* cantrip. When you reach 3rd level, you can cast the *hellish rebuke* spell as a 2nd-level spell once with this trait and regain the ability to do so when you finish a long rest. When you reach 5th level, you can cast the *darkness* spell once with this trait and regain the ability to do so when you finish a long rest. Charisma is your spellcasting ability for these spells.',
+      Darkvision:
+        'Thanks to your infernal heritage, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.'
+    },
+    subraces: []
+  }
 ];
 
 module.exports = raceStore;
