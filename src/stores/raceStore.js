3eea95910078d58ca7388beca011c6ab5a283440
tarajeanne <t.jeanne.patel@gmail.com>
2019-11-18 09:44:42 -0600

    refactored half-elf to proper format

--- src/stores/raceStore.js
+++ src/stores/raceStore.js
@@ -453,21 +453,71 @@ const raceStore = [
         name: '',
         variable: true,
         depends_on: 'race',
-        norepeat: true
+        norepeat: true,
+        options: [
+          'animal handling',
+          'athletics',
+          'intimidation',
+          'nature',
+          'perception',
+          'survival',
+          'acrobatics',
+          'stealth',
+          'sleight of hand',
+          'arcana',
+          'history',
+          'investigation',
+          'religion',
+          'insight',
+          'medicine',
+          'deception',
+          'performance',
+          'persuasion'
+        ]
       },
       {
         name: '',
         variable: true,
         depends_on: 'race',
-        norepeat: true
+        norepeat: true,
+        options: [
+          'animal handling',
+          'athletics',
+          'intimidation',
+          'nature',
+          'perception',
+          'survival',
+          'acrobatics',
+          'stealth',
+          'sleight of hand',
+          'arcana',
+          'history',
+          'investigation',
+          'religion',
+          'insight',
+          'medicine',
+          'deception',
+          'performance',
+          'persuasion'
+        ]
       }
     ],
-    traits: {
-      'Fey Ancestry':
-        'You have advantage on saving throws against being charmed, and magic can’t put you to sleep.',
-      'Darkvision.':
-        'Thanks to your elf blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.'
-    },
+    other_prof: [
+      {
+        name: 'Fey Ancestry',
+        variable: false,
+        depends_on: 'race',
+        desc: 'You have advantage on saving throws against being charmed, and magic can’t put you to sleep.',
+      }
+    ],
+    skills_and_features: [
+      {
+        name: 'Darkvision',
+        variable: false,
+        depends_on: 'race',
+        desc: 'You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light.'
+      }
+    ],
     subraces: []
   },
   {
