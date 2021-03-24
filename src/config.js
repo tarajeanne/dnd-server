af66df06f867b9b559b334fe2f594b47d50a0972
tarajeanne <t.jeanne.patel@gmail.com>
2021-03-23 21:10:08 -0500

    fixed connection object in server.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
-    process.env.DATABASE_URL+'?sslmode=require' || 'postgresql://tarapatel@localhost/dnd-creator',
+    process.env.DATABASE_URL || 'postgresql://tarapatel@localhost/dnd-creator',
   TEST_DB_URL:
     process.env.TEST_DB_URL || 'postgresql://tarapatel@localhost/dnd-creator',
   JWT_SECRET: process.env.JWT_SECRET,
